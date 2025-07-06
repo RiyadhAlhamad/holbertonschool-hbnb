49b995a5e646f29d8f9f32f8ba57531ab2b62a36
Riyadh Alhamad <riyadh.it.dev@gmail.com>
2025-07-06 13:25:06 +0000

    update files init.py inside app also update repository.py and facade.py.com

--- part2/hbnb/app/services/facade.py
+++ part2/hbnb/app/services/facade.py
@@ -7,10 +7,10 @@ from app.models.review import Review
 
 class HBnBFacade:
     def __init__(self):
-        self.user_repo = InMemoryRepository()
-        self.place_repo = InMemoryRepository()
-        self.review_repo = InMemoryRepository()
-        self.amenity_repo = InMemoryRepository()
+        self.user_repository = SQLAlchemyRepository(User)  # Switched to SQLAlchemyRepository
+        self.place_repository = SQLAlchemyRepository(Place)
+        self.review_repository = SQLAlchemyRepository(Review)
+        self.amenity_repository = SQLAlchemyRepository(Amenity)
 
     ### Users section###
 
@@ -19,11 +19,11 @@ class HBnBFacade:
         self.user_repo.add(user)
         return user
 
-    def get_user(self, user_id):
-        return self.user_repo.get(user_id)
+    def get_user_by_id(self, user_id):
+        return self.user_repository.get(user_id)
 
     def get_all_users(self):
-        return self.user_repo.get_all()
+        return self.user_repository.get_all()
 
     def get_user_by_email(self, email):
         return self.user_repo.get_by_attribute('email', email)
